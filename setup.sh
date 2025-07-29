5b55e265e6e6f19de12eb6435bf25292d7c309f4
Channakeshava reddy <channakeshavareddywork@gmail.com>
2025-07-29 14:41:02 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"channakeshavareddy12@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-port = $PORT\n\
+port = \$PORT\n\
 " > ~/.streamlit/config.toml
